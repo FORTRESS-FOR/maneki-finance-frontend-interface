63c285796d6ba73553d8ed3984225ffea3053e07
Alvin Yap <leaf.alvin@gmail.com>
2024-02-15 15:58:34 +0800

    chore: test commit

--- src/maneki/modules/tge/TGEPhaseInfo.tsx
+++ src/maneki/modules/tge/TGEPhaseInfo.tsx
@@ -38,14 +38,14 @@ const TGEPhaseInfo = () => {
           }}
         >
           <Trans>
-            For the first 24 hours, 6 million PAW tokens (6% of the supply) are available for
-            whitelist participates at the fixed price of 0.0002 ETH(~$0.36) and the FDV of ~$36m
-            with a market cap of ~$6.8m. Whitelist addresses are selected from historic vault users,
-            traders and contributors. The allocation of each whitelist address depends on the
-            historic activities. First 30 mins of the whitelist phase has a max deposit limit of 1
-            ETH for each address. Participation is on a first come first served basis and will end
-            early if all the allocation is filled. Please note that being whitelisted does not
-            guarantee a spot if all the allocation is filled early.
+            The first 24 hours, 6 million PAW tokens (6% of the supply) are available for whitelist
+            participates at the fixed price of 0.0002 ETH(~$0.36) and the FDV of ~$36m with a market
+            cap of ~$6.8m. Whitelist addresses are selected from historic vault users, traders and
+            contributors. The allocation of each whitelist address depends on the historic
+            activities. First 30 mins of the whitelist phase has a max deposit limit of 1 ETH for
+            each address. Participation is on a first come first served basis and will end early if
+            all the allocation is filled. Please note that being whitelisted does not guarantee a
+            spot if all the allocation is filled early.
           </Trans>
         </Typography>
       </Paper>
